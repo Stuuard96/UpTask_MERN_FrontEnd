53fededc8b2fd5761e06829298a4dba702a2531a
Stuard Quiroz <stuuard96@gmail.com>
2022-11-18 19:31:23 -0500

    add formulario login

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,9 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 export const AuthLayout = () => {
   return (
-    <>
-      <div>AuthLayout</div>
-      <Outlet />
-    </>
+    <main className="container min-h-screen mx-auto p-5 flex justify-center items-center">
+      <div className="md:w-2/3 lg:w-2/5">
+        <Outlet />
+      </div>
+    </main>
   );
 };
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,5 +1,47 @@
 import React from 'react';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 text-center font-black text-4xl sm:text-5xl capitalize">
+        Inicia sesión y administra tus{' '}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+      <form className="mt-8 bg-white shadow rounded-lg p-7 md:p-10">
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block uppercase text-gray-600 text-lg font-bold"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="w-full p-3 mt-2 border border-slate-300 rounded-lg  focus:outline-none bg-gray-50 focus:bg-white focus:border-sky-500"
+            placeholder="Email de Registro"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="block uppercase text-gray-600 text-lg font-bold"
+          >
+            Email
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="w-full p-3 mt-2 border border-slate-300 rounded-lg  focus:outline-none bg-gray-50 focus:bg-white focus:border-sky-500"
+            placeholder="Password de Registro"
+          />
+        </div>
+        <input
+          type="submit"
+          className="bg-sky-600 hover:bg-sky-700 w-full rounded-lg p-3 text-white uppercase font-bold cursor-pointer transition-colors duration-300"
+          value="Iniciar Sesión"
+        />
+      </form>
+    </>
+  );
 };
